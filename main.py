70ca2c33f7e38d616dd2508229acc3d1fb71c1bd
jonnaliesel <jonnaliesel@gmail.com>
2021-10-11 10:40:19 +0200

    add octagon area function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 def cube():
     while True:
         try:
@@ -8,18 +10,25 @@ def cube():
         except NameError:
             print("Please insert number values, for example 10, 25.5 , 100 etc.")
 
+def octagon_area():
+    octagon_side = eval(input(f'\nInsert octagon side length in centimeters '))
+    octagon_area = 2*octagon_side*2(1+math.sqrt(2))
+
+    octagon_output = f'\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\nThe area of your octagon is {octagon_area} cm^2\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+    return octagon_output
+
 #Här under hittar ni era namn där ni ska ange funktionen ni vill anropa som ni får definiera här ovanför.
 #ändra choice text strängen med er valda objekt.
 
 while True:
     try:
-        choice = eval(input("1: Cube\n2:Carl\n3:Jonna\n4:Ian\n5:Erik\nVolume calculator, input choice:"))
+        choice = eval(input("1: Cube\n2:Carl\n3:Octagon\n4:Ian\n5:Erik\nVolume calculator, input choice:"))
         if choice == 1:
             cube()
         elif choice == 2:
             Carl()
         elif choice == 3:
-            Jonna()
+            print(octagon_area())
         elif choice == 4:
             Ian()
         elif choice == 5:
